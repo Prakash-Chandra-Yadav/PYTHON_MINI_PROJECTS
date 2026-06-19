1e4c70c5b867fb3c26eeb22eb49c9b5f0410f0cc
Prakash Chandra <prakashworks98@gmail.com>
2026-06-19 12:38:49 +0545

    feat: added the new method logcs to view the list of the student and search for the specific student

--- student_app.py
+++ student_app.py
@@ -11,4 +11,20 @@ class Student:
         self.roll_no = roll_no
         self.marks = {}
         self.student = {name:{roll_no:self.roll_no, self.marks:{}}}
+        self.students = {}
+    #method to add the student
+    def add_student(self,name,roll_no):
+        '''add the new student'''
+        self.student[self.name] = {'roll_no':roll_no, 'marks':{}}
+    #method to view all the student
+    def view_all_studnets(self): 
+        '''view all the studnet in the system'''
+        i = 1
+        students = json.load(r'students.json')
+        for student in students.keys():
+            print(f"{i}: {student}")
+            i += 1
+    
+
+
 
