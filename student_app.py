3c61304f092cf38701cc1bbad728758a59e43a1a
Prakash Chandra <prakashworks98@gmail.com>
2026-06-19 13:07:44 +0545

    feat: added the logic to search for the student with exxception handling

--- student_app.py
+++ student_app.py
@@ -24,7 +24,28 @@ class Student:
         for student in students.keys():
             print(f"{i}: {student}")
             i += 1
+    #add metho to serahc student by their name
+    def search_student(self):
+        '''search student by their name'''
+        name = input("please ente the name of the student").title()
+        try:
+            students = json.load(r'students.json')
+            if name in students:
+                print("----student found---")
+                print("--HERE ARE THE DETAILS----")
+                print(f"\n name: {name}\n 'roll: {students[name]['roll']}")
+                print("\n---MARKS---")
+                for key,value in students[name]['marks'].items():
+                    print(f"{key} : {value}")
+            else: 
+                print("Student not found")
+        except ValueError:
+            print("please enter the  name of the student!! number is not allowed")
+    #add the method to update the student information
     
+
+
+
 
 
 
