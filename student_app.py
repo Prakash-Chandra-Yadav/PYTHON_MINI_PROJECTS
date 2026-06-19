a0779598a06e01a36445bf37ea2417eca74ccb4e
Prakash Chandra <prakashworks98@gmail.com>
2026-06-19 12:26:50 +0545

    feat: cerated the new student class

--- student_app.py
+++ student_app.py
@@ -1,3 +1,14 @@
 #import the required libraries
 from pathlib import path 
-import json 
+import json
+
+from pytest import mark 
+
+class Student:
+    def __init__(self,name,roll_no):
+        '''initialize the needed attributes'''
+        self.name = name
+        self.roll_no = roll_no
+        self.marks = {}
+        self.student = {name:{roll_no:self.roll_no, self.marks:{}}}
+
