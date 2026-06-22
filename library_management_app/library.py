f8b9984ab0e3e075d17bbe5cac61be53bb7d0853
Prakash Chandra <prakashworks98@gmail.com>
2026-06-22 17:00:20 +0545

    fix: refactored the logic of the add book

--- library_management_app/library.py
+++ library_management_app/library.py
@@ -19,14 +19,18 @@ class Libraray:
         title = input("enter the title of the book: ")
         author = input("enter the name of the author: ")
         book_id = input("enter the book id: ")
-        total_copies = int(input("total copies owned by libraries: "))
-        available_copies = int(input("enter the number of the copies available: "))
-        borrower = input("enter the name of the brower: ")
+        try: 
+            total_copies = int(input("total copies owned by libraries: "))
+            available_copies = int(input("enter the number of the copies available: "))
+        except ValueError: 
+            print("integere value only allowed")
+        else:
+            borrower = input("enter the name of the brower: ")
 
-        library[book_id] ={'title': title, 'author': author, 'total_copies':total_copies, 'available_copies': available_copies, 'borrowed_by':[borrower,] }
+            library[book_id] ={'title': title, 'author': author, 'total_copies':total_copies, 'available_copies': available_copies, 'borrowed_by':[borrower,] }
 
-        contents = json.dumps(library)
-        self.path.write_text(contents)
+            contents = json.dumps(library)
+            self.path.write_text(contents)
 
     #create the helper function for creating the path
     def _load_path(self):
@@ -39,5 +43,10 @@ class Libraray:
             contents = json.dumps(library)
             path.write_text(contents)
     
-    if __name__ == '__main__':
+def main():
+    l1 = Libraray()
+    l1.add_new_book()
+
+if __name__ == '__main__':
+    main()
         
